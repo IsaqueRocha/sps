bf234503adca18b93c9220a1ada9360c3eca4489
Isaque Rocha <isaquerocha@gmail.com>
2021-03-15 10:32:07 -0300

    test: add feature test to user model

--- www/database/factories/UserFactory.php
+++ www/database/factories/UserFactory.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+use function PHPSTORM_META\type;
+
 class UserFactory extends Factory
 {
     /**
@@ -22,12 +24,26 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $faker = \Faker\Factory::create('pt_BR');
+
+        $array_type = ['seller', 'customer'];
+        $user_type = $array_type[array_rand($array_type)];
+        $type = '\\App\\Models\\' . ucfirst($user_type);
+
+        if ($user_type === 'seller') {
+            $typeable = $type::create(['cnpj' => $faker->cnpj]);
+        } else {
+            $typeable = $type::create(['cpf' => $faker->cpf]);
+        }
+
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'typeable_id' => $typeable->id,
+            'typeable_type' => get_class($typeable)
         ];
     }
 
---
+++ www/tests/Feature/Models/UserTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Tests\TestCase;
+use App\Models\User;
+use Ramsey\Uuid\Uuid;
+use App\Models\Seller;
+use App\Models\Customer;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+    use RefreshDatabase;
+    use WithFaker;
+
+    private $testKeys;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = $this->makeFaker('pt_BR');
+
+        $this->testKeys = [
+            'id',
+            'name',
+            'email',
+            'email_verified_at',
+            'password',
+            'typeable_id',
+            'typeable_type',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+            'remember_token'
+        ];
+    }
+
+    public function testList()
+    {
+        User::factory(10)->create();
+        $users = User::all();
+        $this->assertCount(10, $users);
+
+        $this->assertUserFields($users->first());
+    }
+
+    public function testCreate()
+    {
+        $user = User::factory()->create(['name' => 'New User']);
+        $user->refresh();
+
+        $this->assertTrue(Uuid::isValid($user->id));
+        $this->assertEquals('New User', $user->name);
+        $this->assertNotNull($user->typeable_id);
+        $this->assertNotNull($user->typeable_type);
+        $this->assertUserFields($user);
+
+        $customer = Customer::create(['cpf' => $this->faker->cpf]);
+
+        $user = User::factory()->create([
+            'typeable_id' => $customer->id,
+            'typeable_type' => get_class($customer)
+        ]);
+
+        $this->assertTrue(Uuid::isValid($user->id));
+        $this->assertEquals($customer->id, $user->typeable_id);
+        $this->assertEquals(Customer::class, $user->typeable_type);
+    }
+
+    public function testUpdate()
+    {
+        $customer = Customer::create(['cpf' => $this->faker->cpf]);
+
+        /** @var User $user */
+        $user = User::factory()->create([
+            'typeable_id' => $customer->id,
+            'typeable_type' => get_class($customer)
+        ]);
+
+        $data = [
+            'name' => 'Isaque Rocha',
+            'email' => 'isaque@gmail.com'
+        ];
+
+        $user->update($data);
+
+        foreach ($data as $key => $value) {
+            $this->assertEquals($value, $user->{$key});
+        }
+
+        $seller = Seller::create(['cnpj' => $this->faker->cnpj]);
+
+        $data = [
+            'typeable_id' => $seller->id,
+            'typeable_type' => get_class($seller)
+        ];
+
+        $user->update($data);
+        foreach ($data as $key => $value) {
+            $this->assertEquals($value, $user->{$key});
+        }
+    }
+
+    public function testDelete()
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+        $type = $user->typeable_type;
+
+        $user->delete();
+
+        $this->assertNull(User::find($user->id));
+        $this->assertNull($type::find($user->typeable_id));
+    }
+
+    private function assertUserFields(User $user)
+    {
+        $userKeys = array_keys($user->getAttributes());
+        if (strcmp($user->typeable_type, Customer::class) == 0) {
+            $this->testKeys[] = 'cpf';
+        } else {
+            $this->testKeys[] = 'cnpj';
+        }
+
+        $this->assertEqualsCanonicalizing($this->testKeys, $userKeys);
+    }
+}
