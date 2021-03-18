e95aa2a8782436474ea405b107a15f2e1420efcf
Isaque Rocha <isaquerocha@gmail.com>
2021-03-17 21:08:27 -0300

    chore: remove hardcoded links

--- www/app/Observers/TransactionObserver.php
+++ www/app/Observers/TransactionObserver.php
@@ -16,7 +16,7 @@ class TransactionObserver
     public function created(Transaction $transaction)
     {
         // TODO: Simular mensageria(?)
-        $uri = 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04';
+        $uri = env('TRANSACTION_SERVIME_MESSENGER');
         Http::retry(3, 100)->get($uri, ['user' => $transaction->payee]);
     }
 }
--- www/app/Services/TransactionService.php
+++ www/app/Services/TransactionService.php
@@ -8,7 +8,7 @@ class TransactionService
 {
     public function getPermission()
     {
-        $response = Http::retry(5, 100)->get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
+        $response = Http::retry(5, 100)->get(env('TRANSACTION_SERVICE_AUTHORIZER'));
 
         if ($response->json('message') === 'Autorizado') {
             return true;
